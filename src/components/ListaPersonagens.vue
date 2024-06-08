a496ac5dd6237762040f72f9801bceaa0d4aa826
João Pedro Granciere <127800019+jgranciere@users.noreply.github.com>
2024-06-07 21:36:38 -0300

    Add files via upload

---
+++ src/components/ListaPersonagens.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+
+defineProps([
+    "personagem"
+])
+
+</script>
+
+<template>
+    <div class="col">
+        <div class="card m-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img :src="personagem.image" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8" style="background-color: #052405;">
+                    <div class="card-body" style="background-color: #052405;">
+                        <h5 class="card-title" style="color: white;">{{ personagem.name }}</h5>
+                        <p class="card-text" style="color: white;">
+                            Status: {{ personagem.status }} <br>
+                            Espécie: {{ personagem.species }} <br>
+                            Gênero: {{ personagem.gender }} <br>
+                            Local: {{ personagem.location.name }} <br>
+                            Total De episódios: {{ personagem.episode.length }} <br>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h3>Nome: Carlos Eduardo Colombo</h3>
+    <h3>RA: 1976794</h3>
+    <h3>Faculdade: Ánalise e Desenvolvimento de Sistemas</h3>
+    <h3>Universidade: Unimar</h3>
+  </div>
+</template>
+
+<style>
+  .about{
+    color: white;
+    text-align: center;
+    margin-top: 15%;
+
+  }
+
+</style>
+
+
